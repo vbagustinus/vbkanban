589678ca1df171a35f5990880adc181ab7563713
vbagustinus <vbagustinus@gmail.com>
2017-12-02 15:48:11 +0700

    install iView

--- src/main.js
+++ src/main.js
@@ -4,7 +4,10 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import VueFire from 'vuefire'
+import iView from 'iview'
+import 'iview/dist/styles/iview.css'
 
+Vue.use(iView)
 Vue.use(VueFire)
 Vue.config.productionTip = false
 
