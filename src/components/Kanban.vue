1ad92445b5bd1db000a5f53c082d9488601b8ffa
vbagustinus <vbagustinus@gmail.com>
2017-12-02 15:40:12 +0700

    Fix Task DOne

--- src/components/Kanban.vue
+++ src/components/Kanban.vue
@@ -1,32 +1,32 @@
 <template>
-   <div class="container">
-    <div class="row">
-      <div class="col-md-12">
-        <h1 style="color:#000 !important;">KANBAN-APP</h1>
+   <div class='container'>
+    <div class='row'>
+      <div class='col-md-12'>
+        <h1 style='color:#000 !important;'>KANBAN-APP</h1>
         <hr>
-        <!-- <div class="col-md-10">
+        <!-- <div class='col-md-10'>
         </div> -->
-        <div class="col-md-offset-10 col-md-1">
+        <div class='col-md-offset-10 col-md-1'>
           <!-- Trigger the modal with a button -->
-          <button type="button" style="margin-left: 80%;"class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">+ ADD</button>
+          <button type='button' style='margin-left: 80%;'class='btn btn-info btn-lg' data-toggle='modal' data-target='#myModal'>+ ADD</button>
           <h1></h1>
         </div>
         <!-- task -->
-        <div class="col-md-3">
-          <div class="row">
-            <div class="col-sm-12">
-              <div class="thumbnail" >
-                <h1 style="background-color:red;">Back Log</h1>
-
-                <div class="thumbnail" v-for="task in showtasks" v-if="task.status == 0">
-                  <div class="caption">
+        <div class='col-md-3'>
+          <div class='row'>
+            <div class='col-sm-12'>
+              <div class='thumbnail' >
+                <h1 style='background-color:red;'>Back Log</h1>
+                <div class='thumbnail' v-for='task in showtasks' v-if='task.status == 0'>
+                  <div class='caption'>
                     <div>
                       <h3><b>{{ task.title }}</b></h3>
                       <hr>
+                      <p>Desc: {{ task.desc }} </p>
                       <p>Point: {{ task.point }} </p>
                       <p>Assigned To: {{ task.assignTo }} </p>
-                      <!-- <button @click="showDetail()" type="button">Show Detail</button> -->
-                      <button @click="showDetail(task)" type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#modalDetail">Show Detail</button>
+                      <button @click='removeTask(task)' type='button'>Remove</button>
+                      <button @click='toDo(task)' type='button'>Todo</button>
                     </div>
                   </div>
                   <!-- modal DETAIl -->
@@ -37,19 +37,22 @@
           </div>
         </div>
         <!-- TODO -->
-        <div class="col-md-3">
-          <div class="row">
-            <div class="col-sm-12">
-              <div class="thumbnail">
-                <h1 style="background-color:orange;">TO-DO</h1>
-                <div class="thumbnail" v-for="task in showtasks" v-if="task.status == 1">
-                  <div class="caption">
+        <div class='col-md-3'>
+          <div class='row'>
+            <div class='col-sm-12'>
+              <div class='thumbnail'>
+                <h1 style='background-color:orange;'>TO-DO</h1>
+                <div class='thumbnail' v-for='task in showtasks' v-if='task.status == 1'>
+                  <div class='caption'>
                     <div>
                       <h3><b>{{ task.title }}</b></h3>
                       <hr>
+                      <p>Desc: {{ task.desc }} </p>
                       <p>Point: {{ task.point }} </p>
                       <p>Assigned To: {{ task.assignTo }} </p>
-                      <button @click="showDetail(task)" type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#modalDetail">Show Detail</button>
+                      <button @click='backLog(task)' type='button'>Back Log</button>
+                      <button @click='removeTask(task)' type='button'>Remove</button>
+                      <button @click='doIng(task)' type='button'>Doing</button>
                     </div>
                   </div>
                 </div>
@@ -58,19 +61,22 @@
           </div>
         </div>
         <!-- DOING -->
-        <div class="col-md-3">
-          <div class="row">
-            <div class="col-sm-12">
-              <div class="thumbnail">
-                <h1 style="background-color:#b3f442;">DOING</h1>
-                <div class="thumbnail" v-for="task in showtasks" v-if="task.status == 2">
-                  <div class="caption">
+        <div class='col-md-3'>
+          <div class='row'>
+            <div class='col-sm-12'>
+              <div class='thumbnail'>
+                <h1 style='background-color:#b3f442;'>DOING</h1>
+                <div class='thumbnail' v-for='task in showtasks' v-if='task.status == 2'>
+                  <div class='caption'>
                     <div>
                       <h3><b>{{ task.title }}</b></h3>
                       <hr>
+                      <p>Desc: {{ task.desc }} </p>
                       <p>Point: {{ task.point }} </p>
                       <p>Assigned To: {{ task.assignTo }} </p>
-                      <button @click="showDetail(task)" type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#modalDetail">Show Detail</button>
+                      <button @click='toDo(task)' type='button'>Todo</button>
+                      <button @click='removeTask(task)' type='button'>Remove</button>
+                      <button @click='doNe(task)' type='button'>Done</button>
                     </div>
                   </div>
                 </div>
@@ -79,19 +85,21 @@
           </div>
         </div>
         <!-- DONE -->
-        <div class="col-md-3">
-          <div class="row">
-            <div class="col-sm-12">
-              <div class="thumbnail">
-                <h1 style="background-color:green;">DONE</h1>
-                <div class="thumbnail" v-for="task in showtasks" v-if="task.status == 3">
-                  <div class="caption">
+        <div class='col-md-3'>
+          <div class='row'>
+            <div class='col-sm-12'>
+              <div class='thumbnail'>
+                <h1 style='background-color:green;'>DONE</h1>
+                <div class='thumbnail' v-for='task in showtasks' v-if='task.status == 3'>
+                  <div class='caption'>
                     <div>
                       <h3><b>{{ task.title }}</b></h3>
                       <hr>
+                      <p>Desc: {{ task.desc }} </p>
                       <p>Point: {{ task.point }} </p>
                       <p>Assigned To: {{ task.assignTo }} </p>
-                      <button @click="showDetail(task)" type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#modalDetail">Show Detail</button>
+                      <button @click='doIng(task)' type='button'>Doing</button>
+                      <button @click='removeTask(task)' type='button'>Remove</button>
                     </div>
                   </div>
                 </div>
@@ -103,67 +111,36 @@
 
     </div>
     <!-- Modal Add-->
-    <div class="modal fade" id="myModal" role="dialog">
-      <div class="modal-dialog">
+    <div class='modal fade' id='myModal' role='dialog'>
+      <div class='modal-dialog'>
         <!-- Modal content-->
-        <div class="modal-content">
-          <div class="modal-header">
-            <h4 class="modal-title">Input Task</h4>
+        <div class='modal-content'>
+          <div class='modal-header'>
+            <h4 class='modal-title'>Input Task</h4>
           </div>
-          <div class="modal-body">
+          <div class='modal-body'>
             <form>
-              <div class="form-group">
-                <label for="Title" style="text-align='left'">Title:</label>
-                <input v-model="inputTask.title" type="text" class="form-control" id="title">
+              <div class='form-group'>
+                <label for='Title'>Title:</label>
+                <input v-model='inputTask.title' type='text' class='form-control' id='title'>
               </div>
-              <div class="form-group">
-                <label for="Description">Description:</label>
-                <textarea v-model="inputTask.description" class="form-control" rows="5" id="description"></textarea>
+              <div class='form-group'>
+                <label for='Description'>Description:</label>
+                <textarea v-model='inputTask.desc' class='form-control' rows='5' id='description'></textarea>
               </div>
-              <div class="form-group">
-                <label for="Point">Point:</label>
-                <input v-model="inputTask.point" type="text" class="form-control" id="point">
+              <div class='form-group'>
+                <label for='Point'>Point:</label>
+                <input v-model='inputTask.point' type='text' class='form-control' id='point'>
               </div>
-              <div class="form-group">
-                <label for="AssignTo">Assign To:</label>
-                <input v-model="inputTask.assignTo" type="text" class="form-control" id="assign">
+              <div class='form-group'>
+                <label for='AssignTo'>Assign To:</label>
+                <input v-model='inputTask.assignTo' type='text' class='form-control' id='assign'>
               </div>
             </form>
           </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-            <button @click="inputTask" type="button" class="btn btn-default" data-dismiss="modal">Add Task</button>
-          </div>
-        </div>
-
-      </div>
-    </div>
-
-    <!-- modal show detail -->
-    <div v-if="currentTask !== null" class="modal fade" id="modalDetail" role="dialog">
-      <div class="modal-dialog">
-        <!-- Modal content-->
-        <div class="modal-content" style="text-align:'left'" >
-          <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal">&times;</button>
-            <h4 class="modal-title">Detail Task : {{ currentTask.title }} for {{ currentTask.assignTo }} </h4>
-          </div>
-          <div class="modal-body">
-            <form>
-              <div class="form-group">
-                <label for="Title" style="text-align='left'">Task Description: {{ currentTask.description }}</label>
-                <p> Point : {{ currentTask.point }} </p>
-                <p> Status : {{ currentTask.status }}</p>
-              </div>
-            </form>
-          </div>
-          <div class="modal-footer" >
-            <button v-if="currentTask.status == 'todo'" @click="task(currentTask)" type="button" class="btn btn-default danger" data-dismiss="modal">task</button>
-            <button @click="removeTask(currentTask)" type="button" class="btn btn-default danger" data-dismiss="modal">DELETE</button>
-            <button v-if="currentTask.status == 'doing' || currentTask.status == 'task'" @click="todo(currentTask)" type="button" class="btn btn-default" data-dismiss="modal">TO-DO</button>
-            <button v-if="currentTask.status == 'todo'  || currentTask.status == 'done'" @click="doing(currentTask)" type="button" class="btn btn-default danger" data-dismiss="modal">DOING</button>
-            <button v-if="currentTask.status == 'doing'" @click="done(currentTask)" type="button" class="btn btn-default danger" data-dismiss="modal">DONE</button>
-
+          <div class='modal-footer'>
+            <button type='button' class='btn btn-default' data-dismiss='modal'>Close</button>
+            <button @click='saveTask' type='button' class='btn btn-default' data-dismiss='modal'>Add Task</button>
           </div>
         </div>
 
@@ -177,12 +154,8 @@ import * as firebase from 'firebase'
 
 // Initialize Firebase
 var config = {
-  apiKey: 'AIzaSyCBRDUAkXB6G5VtiFzwP0F-J4a8EEETTrM',
-  authDomain: 'vbkanban.firebaseapp.com',
   databaseURL: 'https://vbkanban.firebaseio.com',
-  projectId: 'vbkanban',
-  storageBucket: 'vbkanban.appspot.com',
-  messagingSenderId: '294358815008'
+  projectId: 'vbkanban'
 }
 
 const firebaseApp = firebase.initializeApp(config)
@@ -198,7 +171,7 @@ export default {
         title: null,
         desc: null,
         point: null,
-        assignedTo: null,
+        assignTo: null,
         status: 0
       },
       currentTask: null
@@ -208,14 +181,51 @@ export default {
     showtasks: db
   },
   methods: {
-    showDetail () {
-      console.log('SHOW')
+    saveTask () {
+      db.push(this.inputTask)
+      this.inputTask.title = null
+      this.inputTask.desc = null
+      this.inputTask.point = null
+      this.inputTask.assignTo = null
+      this.inputTask.status = 0
+    },
+    toDo (task) {
+      console.log('TODO')
+      let data = db.child(task['.key'])
+      data.update({
+        'status': 1
+      })
+    },
+    doIng (task) {
+      console.log('DOING')
+      let data = db.child(task['.key'])
+      data.update({
+        'status': 2
+      })
+    },
+    doNe (task) {
+      console.log('DONE')
+      let data = db.child(task['.key'])
+      data.update({
+        'status': 3
+      })
+    },
+    backLog (task) {
+      console.log('BACKLOG')
+      let data = db.child(task['.key'])
+      data.update({
+        'status': 0
+      })
+    },
+    removeTask (task) {
+      console.log('REMOVE')
+      db.child(task['.key']).remove()
     }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+<!-- Add 'scoped' attribute to limit CSS to this component only -->
 <style scoped>
 h1, h2 {
   font-weight: normal;
